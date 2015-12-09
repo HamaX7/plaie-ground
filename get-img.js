c2a816786e4b5dbaa8e39452dd9bb0e446742d9c
Marc <marc.alzetto@gmail.com>
2015-12-09 15:54:59 +0100

    to test

--- get-img.js
+++ get-img.js
@@ -4,17 +4,59 @@
 
     app.controller('GetimgController', function()
     {
+        function isOnePointZero(n) {
+            return typeof n == "string" && n.indexOf('.') != -1 && parseFloat(n) === 1;
+        };
+        function isPercentage(n) {
+            return typeof n === "string" && n.indexOf('%') != -1;
+        };
+        function bound01(n, max) {
+            if (isOnePointZero(n)) { n = "100%"; }
+            var processPercent = isPercentage(n);
+            n = Math.min(max, Math.max(0, parseFloat(n)));
+            if (processPercent) {
+                n = parseInt(n * max, 10) / 100;
+            }
+            if ((Math.abs(n - max) < 0.000001)) {
+                return 1;
+            }
+            return (n % max) / parseFloat(max);
+        };
+        function rgbToHsv(r, g, b) {
+            r = bound01(r, 255);
+            g = bound01(g, 255);
+            b = bound01(b, 255);
+
+            var max = Math.max(r, g, b), min = Math.min(r, g, b);
+            var h, s, v = max;
+
+            var d = max - min;
+            s = max === 0 ? 0 : d / max;
+
+            if(max == min) {
+                h = 0; // achromatic
+            }
+            else {
+                switch(max) {
+                    case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+                    case g: h = (b - r) / d + 2; break;
+                    case b: h = (r - g) / d + 4; break;
+                }
+                h /= 6;
+            }
+            return { h: h, s: s, v: v };
+        }
+
         console.log("check");
         var image = document.getElementById("image-src");
         var canvas = document.createElement('canvas');
         canvas.width = image.width;
         canvas.length = image.length;
         var context = canvas.getContext('2d');
-        context.drawImage(image, 0, 0);
+        
         var imgData = context.getImageData(0, 0, image.width, image.height);
         var pixels = imgData.data;
         var hsvPixels = new Array();
-
         // pixels contient les valeurs RGBA (red, green, blue, alpha chanel ou transparence)
         // de chaque pixel (ex: pixel 1 = pixels[0], pixels[1], pixels[2] et pixels[3])
         //var rgbaPixels = new Array();
@@ -23,40 +65,34 @@
         var nbPixels = widthPixels * heightPixels;*/
         for (i = 0; i < pixels.length; i = i + 4)
         {
-            hsvPixels.push(rgbToHsv(pixels[i], pixels[i + 1], pixels[i + 2]));
-            /*var rgba = new Array();
-            rgba.push(pixels[i], pixels[i + 1], pixels[i + 2], pixels[i + 3]);
-            rgbaPixels.push(rgba);*/
+            var hsv = rgbToHsv(pixels[i], pixels[i + 1], pixels[i + 2]);
+            if (hsv['h'] > 0.44 && hsv['h'] < 0.55
+                && hsv['s'] > 0.2 && hsv['s'] < 0.8
+                && hsv['v'] > 0.2 && hsv['v'] < 0.8)
+            {
+                pixels[i] = 0;
+                pixels[i+1] = 0;
+                pixels[i+2] = 0;
+                pixels[i+3] = 255;
+            }
+            else
+            {
+                pixels[i] = 255;
+                pixels[i+1] = 255;
+                pixels[i+2] = 255;
+                pixels[i+3] = 255;
+            }
         }
-        for (i = 0; i < 10; i++) {
-            console.log(hsvPixels[i]);
-        }
+        /*for (i = 0; i < (5 * 4); i += 4) {
+            console.log(pixels[i]);
+            console.log(pixels[i+1]);
+            console.log(pixels[i+2]);
+        }*/
+        
+        context.drawImage(image, 0, 0);
+        var img = document.createElement("img");
+        img.src = canvas.toDataURL("image/png");
+        document.body.appendChild(img);
     });
 })();
 
-
-function rgbToHsv(r, g, b)
-{
-    r = bound01(r, 255);
-    g = bound01(g, 255);
-    b = bound01(b, 255);
-
-    var max = math.max(r, g, b), min = math.min(r, g, b);
-    var h, s, v = max;
-
-    var d = max - min;
-    s = max === 0 ? 0 : d / max;
-
-    if(max == min) {
-        h = 0; // achromatic
-    }
-    else {
-        switch(max) {
-            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
-            case g: h = (b - r) / d + 2; break;
-            case b: h = (r - g) / d + 4; break;
-        }
-        h /= 6;
-    }
-    return { h: h, s: s, v: v };
-}
